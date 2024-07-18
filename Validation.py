57d0ea390d6c734b4efd2585952cfdf14787f7b8
TranTungDuong1611 <163084612+TranTungDuong1611@users.noreply.github.com>
2024-07-18 18:25:29 +0700

    refactor

--- Validation.py
+++ Validation.py
@@ -1,7 +1,7 @@
 #Using model from YOLOv8
 from ultralytics import YOLO
 
-model_path = 'C:\Users\Admin\Desktop\LearningAI\CarDetection\model\yolov8\detect\train\weights\best.pt'
+model_path = '\CarDetection\model\yolov8\detect\train\weights\best.pt'
 model = YOLO(model_path)
 
 metrics = model.val(
